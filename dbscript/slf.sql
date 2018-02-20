6c28aeff543b417672e0b4d3c1ff97a066b670f5
zico404 <zicomighty@gmail.com>
2018-02-19 18:01:01 -0800

    new db script

--- dbscript/slf.sql
+++ dbscript/slf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2018 at 11:01 AM
+-- Generation Time: Feb 20, 2018 at 02:46 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -46,6 +46,31 @@ INSERT INTO `guest` (`id`, `browser`, `location`, `ip`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(8) NOT NULL,
+  `typeof` varchar(20) NOT NULL,
+  `item_id` varchar(12) NOT NULL,
+  `user_id` varchar(12) NOT NULL,
+  `like_amount` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `typeof`, `item_id`, `user_id`, `like_amount`) VALUES
+(1, 'lodge', '4X7lSo6239', '123ayyyy', 1),
+(2, 'lodge', 'Pp72UN8460', '123ayyyy', 10),
+(3, 'lodge', 'Aal7UI2017', '123ayyyy', 4),
+(4, 'lodge', 'FUXCWD0548', '123ayyyy', 23),
+(5, 'lodge', 'MuQSN4702', '123ayyyy', 11);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lodge`
 --
 
@@ -53,23 +78,45 @@ CREATE TABLE `lodge` (
   `id` int(8) NOT NULL,
   `lodge_id` varchar(12) NOT NULL,
   `lodge_name` varchar(30) NOT NULL,
-  `lodge_img` varchar(30) NOT NULL,
+  `lodge_img` varchar(100) NOT NULL,
   `type_of` varchar(70) NOT NULL,
   `user_id` varchar(12) NOT NULL,
+  `user_role` varchar(10) NOT NULL DEFAULT 'user',
   `state` varchar(20) NOT NULL,
   `lga` varchar(30) NOT NULL,
-  `address` varchar(40) NOT NULL,
+  `approved` int(2) NOT NULL DEFAULT '0',
   `price` int(8) NOT NULL,
-  `purchased` tinyint(1) NOT NULL,
-  `meta` varchar(200) NOT NULL
+  `available` tinyint(1) NOT NULL DEFAULT '1',
+  `meta` varchar(200) NOT NULL,
+  `upload_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lodge`
 --
 
-INSERT INTO `lodge` (`id`, `lodge_id`, `lodge_name`, `lodge_img`, `type_of`, `user_id`, `state`, `lga`, `address`, `price`, `purchased`, `meta`) VALUES
-(1, 'uasDf2098', 'Bet Lodge', 'bg1.jpg', '', '123ayyyy', 'abia', '0', '3 saviours str', 40, 1, 'Beautiful House');
+INSERT INTO `lodge` (`id`, `lodge_id`, `lodge_name`, `lodge_img`, `type_of`, `user_id`, `user_role`, `state`, `lga`, `approved`, `price`, `available`, `meta`, `upload_time`) VALUES
+(9, 'jpPuen5561', 'My lodge 1', '2017_3$largeimg23_Mar_2017_120735618.jpg', '1room', '123ayyyy', 'admin', 'abia', 'Aba North', 0, 0, 1, '1 bedroom,1 toilet and kitchen', '2018-02-16 02:07:35.483477'),
+(10, '4X7lSo6239', 'My lodge 2', '16704349.jpg', 'selfcon', '123ayyyy', 'admin', 'umuahia', 'Aba North', 1, 0, 1, '1 bedroom,1 toilet and kitchen', '2018-02-16 02:07:35.483477'),
+(11, '9uX0HC2519', 'beauty lodge', 'static1.squarespace.com.jpg', 'selfcon', '123ayyyy', 'admin', 'abuja', 'abuja north', 1, 0, 1, '2 bedroom apartment', '2018-02-16 02:07:35.483477'),
+(12, 'Pp72UN8460', 'prestige lodge', '7-future-lynx.jpg', '1room', '123ayyyy', 'admin', 'owerri', 'owerri municipal', 1, 0, 1, '1 room, hospitable environment etc.', '2018-02-16 02:07:35.483477'),
+(13, 'hQf9Yj1241', 'Good news lodge', 'nature-beautiful-grapes-high-definition-full-screen-wallpaper-image-download.jpg', '1room', '123ayyyy', 'admin', 'umuahia', 'ibeku', 1, 0, 1, '1 beautiful room', '2018-02-16 02:07:35.483477'),
+(14, 'Aal7UI2017', 'Mamador lodge', '21682140-fdi-foreign-direct-investment-concept.jpg', 'selfcon', '123ayyyy', 'admin', 'abia', 'aba owerri', 1, 0, 1, 'Beautiful self contain lodge', '2018-02-16 02:07:35.483477'),
+(15, 'H3GLol7529', 'lodge1', '102701709-472995504.530x298.jpg', 'selfcon', '123ayyyy', 'admin', 'abia', 'ideato', 0, 0, 0, '2 rooms and parlour', '2018-02-16 02:07:35.483477'),
+(16, 'FUXCWD0548', 'florida lodge', 'FDI.jpg', '1room', 'Yp76H27853', 'user', 'abia', 'osisioma', 0, 0, 0, 'Hello there this is a little description of my lodge, it is a fine lodge and i hope you will love it', '2018-02-16 02:07:35.483477'),
+(17, 'MuQSN47027', 'kosy lodge', '56292f_4b701296d2574f54b22357cbcb9f4bb2.jpg', '1room', 'Yp76H27853', 'user', 'owerri', 'owerri west', 0, 0, 0, 'best lodge in owerri', '2018-02-16 02:07:35.483477');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(7) NOT NULL,
+  `order_id` varchar(12) NOT NULL,
+  `user_id` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -126,7 +173,8 @@ INSERT INTO `users` (`id`, `uid`, `firstname`, `lastname`, `email`, `gender`, `p
 (15, 'TISlz82195', 'chris', 'jericho', 'chrisjerico@yahoo.com', 'male', '0803247834', 'sokoto', '688981fe5042d9d30fd1ed0c8473a8ed1f782281', 'default.jpg', 'user', '127.0.0.1', 'false', 1517712195),
 (16, 'OQ0FSm6031', 'bvbv', 'hvhbv&#39;', 'zicomighty47@gmail.com', 'female', '77', 'hghuhuhi', '688981fe5042d9d30fd1ed0c8473a8ed1f782281', 'default.jpg', 'user', '127.0.0.1', 'false', 1517716031),
 (17, 'gqhO4K8093', 'anita', 'orji', 'anitaorji@yahoo.com', 'female', '08039423822', 'abia', 'd56c82a0ab1c536999c31ae5e2c0dab85f47a331', 'default.jpg', 'user', '::1', 'false', 1517778093),
-(18, 'BDNmE93339', 'joe', 'west', 'joe@gmail.com', 'male', '09043423', 'imo', '16a9a54ddf4259952e3c118c763138e83693d7fd', 'default.jpg', 'user', '127.0.0.1', 'false', 1517793339);
+(18, 'BDNmE93339', 'joe', 'west', 'joe@gmail.com', 'male', '09043423', 'imo', '16a9a54ddf4259952e3c118c763138e83693d7fd', 'default.jpg', 'user', '127.0.0.1', 'false', 1517793339),
+(19, '0sFJnq6537', 'prince', 'james', 'princejames@yahoo.com', 'male', '097090494393', 'imo', 'a47b5cc8f06168f0ec3832a99894834e1d27f744', 'default.jpg', 'user', '::1', 'false', 1517956537);
 
 -- --------------------------------------------------------
 
@@ -154,9 +202,21 @@ ALTER TABLE `guest`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `lodge`
 --
 ALTER TABLE `lodge`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -187,10 +247,20 @@ ALTER TABLE `user_login`
 ALTER TABLE `guest`
   MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `lodge`
 --
 ALTER TABLE `lodge`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `site_temp`
 --
@@ -200,7 +270,7 @@ ALTER TABLE `site_temp`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `user_login`
 --
