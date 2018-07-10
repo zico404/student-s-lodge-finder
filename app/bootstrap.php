7b04007acf3afdc3777a347d6376215446f9755f
kaperskyguru <solomoneseme@gmail.com>
2018-07-10 16:31:30 +0000

    Register and login functions all fixed,

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
 
-//Load Config
+//Load Config/Helpers
 require_once 'config/config.php';
+require_once 'helpers/session_helper.php';
+require_once 'helpers/functions.php';
+require_once 'helpers/url_helper.php';
+require_once 'helpers/flash_helper.php';
+
+
 
 //AutoLoad libraries
 spl_autoload_register(function ($className) {
